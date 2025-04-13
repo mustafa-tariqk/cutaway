de7b3efe8972191acdc694206ade7318b310d20b
Mustafa Tariq <mustafa.tariqk@icloud.com>
2025-04-13 13:37:45 -0700

    Setting up DB

--- src/cutaway/database/chroma.py
+++ src/cutaway/database/chroma.py
@@ -1,16 +1,24 @@
-import chromadb
+from typing import List
 
-client = chromadb.PersistentClient()
-collection = client.get_or_create_collection(name="videos")
-
-
-def add_embedding(embedding, filepath: str, timestamp: float):
-    collection.add(
-        embeddings=tensor_to_embedding(embedding),
-        metadatas=[{"filepath": filepath, "timestamp": timestamp}],
-        ids=[str(hash(f"{filepath}_{timestamp}"))],
-    )
+from chromadb.api.models.Collection import Collection
+from chromadb.base_types import Vector
+from chromadb import PersistentClient
 
 
-def tensor_to_embedding(tensor):
-    return tensor.tolist()
+class DB:
+    def __init__(self, path_to_videos: str):
+        self.collection: Collection = PersistentClient().get_or_create_collection(
+            name=path_to_videos
+        )
+
+    def add_video_embedding(
+        self, embeddings: Vector, filepath: str, timestamps: List[float]
+    ):
+        self.collection.add(
+            embeddings=embeddings,
+            metadatas=[
+                {"filepath": filepath, "timestamp": timestamp}
+                for timestamp in timestamps
+            ],
+            ids=[str(hash(f"{filepath}_{timestamp}")) for timestamp in timestamps],
+        )
